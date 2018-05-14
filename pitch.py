763a967cec878bc6b801ac7f03c8023608faaa1e
Arianna Masciolini <uzkamascio@gmail.com>
2018-05-14 15:46:53 +0200

    Delete scripts.

--- pitch.py
+++
@@ -1,4 +0,0 @@
-import aud
-import soundtrack as st
-
-st.sound_handle.pitch = 2   # currently a mess, but promising
--- reverse.py
+++
@@ -1,4 +0,0 @@
-import aud
-import soundtrack as st
-
-st.sound_handle = device.play(st.sound.reverse) # currently a mess, but promising
