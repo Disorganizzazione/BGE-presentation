6493d117105471a74b77f2e781abc37eab9052d8
Shizen39 <mazzagiorgio96@gmail.com>
2018-05-17 16:07:50 +0200

    Grab script

--- GrabAndMove.py
+++ GrabAndMove.py
@@ -3,31 +3,23 @@ from mathutils import Vector
 
 def main():
 
-	cont = bge.logic.getCurrentController()
-	own = cont.owner
-	Click = cont.sensors['lclick']
+	cont = bge.logic.getCurrentController() #The Python controller of this Python script.
+	own = cont.owner #The object the controller is attached to.
+	Click = cont.sensors['lclick'] 
 	
 	if Click.positive:
 		 cam = bge.logic.getCurrentScene().active_camera
+		 offset = cam.worldPosition + (cam.worldOrientation*Vector([0,0,-20])) #camera offset
 		 #Pick a object
-		 if type(own['Grabbed']) is str:
-			 offset = cam.worldPosition+(cam.worldOrientation*Vector([0,0,-own['Range']]))
-             #collisiom
-			 if cam.parent:
-				 Ray = cam.parent.rayCast(offset, cam.worldPosition,0)
-			 else:
-				 Ray = cam.rayCast(offset, cam.worldPosition,0)
-				 
-			 if Ray[0]:
-				 print('Hit')
-				 own['Grabbed']=Ray[0]
-		 else:
-			 #Move The Object
-			 offset = cam.worldPosition+(cam.worldOrientation*Vector([0,0,-20]))
-			 V=offset-own['Grabbed'].worldPosition
+		 if type(own['Grabbed']) is str: #Hit the object
+			 Ray = cam.rayCast(offset) #Find first object hit within offset and (self) object center
+			 if Ray[0]: #if collided
+				 own['Grabbed']=Ray[0] #print('Hit', Ray[0])
+		 else: #Move The Object
+			 V= offset - own['Grabbed'].worldPosition #camera-grabbed object offset
 			 own['Grabbed'].applyForce((V)*9.8*V.magnitude)
-			 own['Grabbed'].worldLinearVelocity*=0.9	 
+			 own['Grabbed'].worldLinearVelocity*=0.9
 	else:
 		own['Grabbed']='Empty'
-		
+
 main()
